efe225d3b7afaf85d266a2d9835a6a5b83d73fac
Parping <74092171+Parping@users.noreply.github.com>
2024-04-03 21:20:38 -0400

    shake effect when hit the enemy

--- enemy.gd
+++ enemy.gd
@@ -11,6 +11,7 @@ var targetposition
 @onready var player = get_parent().get_node("theRat")
 @export var Item = preload("res://item.tscn")
 @onready var effects = $Effect
+@onready var shake = get_parent().get_node("Camera2D")
 
 var jerk_time = 1
 var jerk_lower_bound = -0.2
@@ -18,6 +19,7 @@ var jerk_upper_bound = 0.2
 var noise = Vector2(randf_range(jerk_lower_bound, jerk_upper_bound), randf_range(jerk_lower_bound, jerk_upper_bound))
 var speed_boost = 0
 var attacking = false
+var deltax=0# for the shake
 
 @export var experience = 150
 
@@ -25,7 +27,7 @@ func _ready():
 	$JerkTimer.start(jerk_time)
 
 func _physics_process(delta):
-	
+	deltax=delta#store the value for shake
 	playerposition = player.position
 	targetposition = (playerposition - position).normalized()
 
@@ -98,6 +100,9 @@ func _on_hurt_box_area_entered(area):
 
 func get_damage():
 	effects.play("gethurt")
+	shake.shake_change()
+	shake._process(deltax)
+	shake.shake_false()
 	$Timer.start(0.4)
 	if(health <= 0):
 		death()
