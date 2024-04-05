e6d2fdc668a23bae7b1db9299901533dba6c9c64
Parping <74092171+Parping@users.noreply.github.com>
2024-04-05 16:10:57 -0400

    trap graph update

--- trap.gd
+++ trap.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 var damage = 30
-
+@onready var animation=$Sprite2D/AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	animation.play("default")
 	pass # Replace with function body.
 
 
