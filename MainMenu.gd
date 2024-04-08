4cbd4ff8411f11675f444b742d706cd2c40686fb
Parping <74092171+Parping@users.noreply.github.com>
2024-04-08 19:11:34 -0400

    score_board

--- MainMenu.gd
+++ MainMenu.gd
@@ -36,35 +36,56 @@ func start_effect():
 	get_tree().root.add_child(simultaneous_scene)
 
 
+func update_EnemyKill(score:int):
+	$ScoreBoard/bigScoreRect/scoreRect/EnemyKill/score.text=str(score)
+	pass
+
+func update_Damage(score:int):
+	$ScoreBoard/bigScoreRect/scoreRect/Damage/score.text=str(score)
+
+func update_HPS(score:int):
+	$ScoreBoard/bigScoreRect/scoreRect/HPS/score.text=str(score)
+func update_Boss(score:int):
+	$ScoreBoard/bigScoreRect/scoreRect/boss/score.text=str(score)
+
+func update_Total(score:int):
+	$ScoreBoard/bigScoreRect/scoreRect/Total_Mark/score.text=str(score)
+	
+func update_Label(value:String):
+	$Label.text=value
+
+func update_Logs(value:int):
+	$ScoreBoard/bigScoreRect/Logs.text="Logs:"+str(value)
+
 func game_over():
 	$ScoreBoard.visible=true
 	$gameover_music.play()
+	score_tween()
+	
+
+
+	
+	
+func score_tween():
 	var scoretween=get_tree().create_tween()
+	scoretween.parallel().tween_method(update_EnemyKill, 0, 3000, 1)
+	scoretween.parallel().tween_method(update_Damage, 0, 400, 1)
+	scoretween.parallel().tween_method(update_HPS, 0, 30, 1)
+	scoretween.parallel().tween_method(update_Boss, 0, 2, 1)
+	scoretween.tween_method(update_Total, 0, 99999, 1)
+	$ScoreBoard/bigScoreRect/Logs.visible=true
+	scoretween.tween_method(update_Logs, 0, 100, 1).set_delay(0.5).set_ease(Tween.EASE_IN_OUT)
+	$ScoreBoard/bigScoreRect/Close_button.show()
+	
 
-	await scoretween.tween_property($ScoreBoard/bigScoreRect/scoreRect/score, "text", "SOCRE:30!", 3).finished
-	$ScoreBoard.visible=false
-	
-	
-	
-	
 
-
-	
+func game_over_tween():
 	Title_show_message("Game Over")
 	var tweenword=get_tree().create_tween()
 	tweenword.set_ease(Tween.EASE_IN_OUT)
-	
-	
-	
 	$StartButton.hide()
-	# Wait until the MessageTimer has counted down.
-	#await $MessageTimer.timeout
-	await tweenword.tween_interval(0.5)
-	await tweenword.tween_property($Label, "text", "Rat Knight!", 1.5).finished
+	tweenword.tween_method(update_Label, "Game Over", "Rat Knight!", 1.5)
 	
-	
-	#$Label.text = "Rat Knight!"
-	# Make a one-shot timer and wait for it to finish.
 	$StartButton.set_text ("TRY AGAIN!")
 	$StartButton.self_modulate.a=0
 	$StartButton.show()
@@ -73,11 +94,16 @@ func game_over():
 	await tween.tween_property($StartButton, "self_modulate:a", 1, 1.5).finished
 	tween.tween_callback($StartButton.queue_free)
 	
-	#await get_tree().create_timer(0.3).timeout
-
-
+	
+	pass	
 
 
 func _on_message_timer_timeout():
 	#$Message.hide()
 	pass
+
+
+func _on_close_button_pressed():
+	$ScoreBoard.visible=false
+	game_over_tween()
+	pass # Replace with function body.
